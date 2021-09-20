0f63cc09626e370f17fe44121f6bffca8133aabf
Angelic Sparkle <76914670+Angelic114@users.noreply.github.com>
2021-09-20 12:12:58 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const ts = document.getElementById("ts")
+const pe = document.getElementById("pe")
+const back = document.getElementsByClassName("back")
+
+ts.onclick=()=>{
+    location.href = "./ingredients.html"
+}
+
+pe.onclick=()=>{
+    location.href = "./recipe.html"
+}
+
+back.onclick=()=>{
+    location.href = "./index.html"
+}
